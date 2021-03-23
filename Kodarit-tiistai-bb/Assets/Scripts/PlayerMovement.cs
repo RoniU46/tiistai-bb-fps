6a328a30e61a3f69b9732506209f1cfa76d93224
roniu <roniunnaslahti2@gmail.com>
2021-03-23 19:28:16 +0200

    weapon scripti

--- Kodarit-tiistai-bb/Assets/Scripts/PlayerMovement.cs
+++ Kodarit-tiistai-bb/Assets/Scripts/PlayerMovement.cs
@@ -11,8 +11,12 @@ public class PlayerMovement : MonoBehaviour
     private float jumpHeight = 5f;
 
     public Transform groundCheck;
-    private float groundDistance = 0.2f;
+    private float groundDistance = 0.4f;
     private bool isGrounded;
+
+    public LayerMask ground;
+
+    private Vector3 velocity;
 
     // Start is called before the first frame update
     void Start()
@@ -23,11 +27,24 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, ground);
+
         float xAxis = Input.GetAxis("Horizontal");
         float zAxis = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * xAxis + transform.forward * zAxis;
 
         controller.Move(move * moveSpeed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
     }
+
 }
